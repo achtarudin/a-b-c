a41eaeda2f39569ad5af7b034b2a757885c5845e
vagrant <vagrant@homestead>
2018-09-25 05:48:07 +0000

    setup app

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Achtarudin-Port</title>
+    {!! Html::style('css/app.css') !!}
+    <style>
+    body{
+        background-color: #fff; 
+        background-image: 
+        linear-gradient(90deg, transparent 79px, #abced4 79px, #abced4 81px, transparent 81px),
+        linear-gradient(#eee .1em, transparent .1em);
+        background-size: 100% 1.2em;
+        height: 100vh;
+    }
+    </style>
+</head>
+<body>  
+    @yield('content')
+    {!! Html::script('js/app.js') !!}
+</body>
+</html>
---
+++ resources/views/page/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+@section('content')
+    <div class="container">
+      <div class="bg">
+        oke
+        <button class="btn btn-primary btn-small">test</button>
+      </div>
+    </div>
+@endsection
